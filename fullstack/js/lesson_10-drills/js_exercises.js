a34f38f125f6a1f585dd0b6520e53bcbb9bde0ae
tomer79sagi <me@tomersagi.com>
2022-12-11 19:01:14 +0200

    Array and functions

---
+++ fullstack/js/lesson_10-drills/js_exercises.js
@@ -0,0 +1,18 @@
+// Function receives 2 paramters (2 arrays)
+function task_3(array1, array2) {
+    for (let i=0 ; i<array2.length ; i++) {
+
+        // Add element from array2 at the end of array1
+        array1.push(array2[i]);
+    }
+
+    return array1;
+}
+
+function task_3_test() {
+    const a1 = [1,5,7,4,33134];
+    const a2 = ["Tomer", "Sagi", 4, 46,46];
+    const a3 = task_3_combine_arrays(a1, a2);
+
+    document.write(a3);
+}
