c2e0a6884dbd223d30d862427c06dff32a5eae23
tomer79sagi <me@tomersagi.com>
2022-12-21 18:20:35 +0200

    enum + class exercise

---
+++ fullstack/react/lesson_1/hello_world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PageHeader from './components/pageHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <PageHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstack/react/lesson_1/hello_world/src/components/pageHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageHeader = () => {
+    return ( 
+        <React.Fragment>
+            PageHeader Component Works!
+        </React.Fragment>
+
+     );
+}
+ 
+export default PageHeader;
