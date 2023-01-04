edf133d6236c24f83398fce3b0a367fefec1d362
tomer79sagi <me@tomersagi.com>
2023-01-04 21:21:42 +0200

    React, state, class components

--- fullstack/react/lesson_1/hello_world2/src/App.js
+++ fullstack/react/lesson_1/hello_world2/src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Clock from "./components/__clock";
+import Home from "./components/Home";
 
 function App() {
 
-  if (2 > 1) {
-    // for (let i=0; i<)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          George Washington
-        </a>
-      </header>
+    <div>
+      <Home/>
     </div>
   );
 }
---
+++ fullstack/react/lesson_1/hello_world2/src/components/Home.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import PageHeader from './PageHeader';
+import './Home.css';
+
+class Home extends Component {
+
+    state = { 
+        full_name: "Tomer Sagi",
+        phone_number: "0528684411",
+        email: "me@tomersagi.com"
+    }
+
+    myStyles = {
+        backgroundColor: "green"
+    }
+
+    getName() {
+        // this.state.full_name = "John"; // FORBIDDEN
+
+        setTimeout(() => {
+            this.setState({
+                full_name: "John Smith"
+            });
+        }, 3000);
+
+        return "Tomer";
+    }
+    
+    render() { 
+        return (
+            <div>
+                <PageHeader/>
+                <div style={ this.myStyles }>{ this.state.full_name }</div>
+                <div style={ {backgroundColor: "orange"} }>{ this.getName() }</div>
+            </div>
+        );
+    }
+}
+ 
+export default Home;
