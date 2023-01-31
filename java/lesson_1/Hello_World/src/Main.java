38494161a05c01babe10a6864af0911abc751e40
tomer79sagi <me@tomersagi.com>
2023-02-01 01:41:38 +0200

    First commit

---
+++ java/lesson_1/Hello_World/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // -- Lesson 1
+        boolean isFlag = true;
+        char myChar = 't'; // Accepts only one character
+        byte myByte = 127;
+        short myShortNum = 867;
+        int myNum1 = 22312412;
+        long myLong = 3432424343232L;
+
+        float myFloat = 3.24f;
+        double myDouble = 5342342342342323423423423234234151532342342341.34342342;
+        float a = 4/5;
+
+        String name = "abc";
+        System.out.println(isFlag + "," + myChar + "," + myByte);
+
+        // -- Lesson 2
+        // Modulus
+
+        // Math.random()
+    }
+}
